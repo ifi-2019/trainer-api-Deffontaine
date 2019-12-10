9b0c0ca9ac1458eec036a2c300ba64012562ac87
Deffontaine Marine <deffontaine@b11p11.fil.univ-lille1.fr>
2019-12-10 09:23:28 +0100

    Tp security jusque 4.5

--- src/test/java/com/ifi/trainer_api/controller/TrainerControllerIntegrationTest.java
+++ src/test/java/com/ifi/trainer_api/controller/TrainerControllerIntegrationTest.java
@@ -1,15 +1,22 @@
 package com.ifi.trainer_api.controller;
 
+import com.ifi.trainer_api.bo.Pokemon;
 import com.ifi.trainer_api.bo.Trainer;
 
+import com.ifi.trainer_api.repository.TrainerRepository;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -25,6 +32,44 @@ class TrainerControllerIntegrationTest {
     @Autowired
     private TrainerController controller;
 
+    @Value("${spring.security.user.name}")
+    private String username;
+
+    @Value("${spring.security.user.password}")
+    private String password;
+
+    @Autowired
+    private TrainerRepository repository;
+
+    @BeforeEach
+    void before(){
+        var ash = new Trainer("Ash");
+        var pikachu = new Pokemon(25, 18);
+        ash.setTeam(List.of(pikachu));
+
+        var misty = new Trainer("Misty");
+        var staryu = new Pokemon(120, 18);
+        var starmie = new Pokemon(121, 21);
+        misty.setTeam(List.of(staryu, starmie));
+
+        // save a couple of trainers
+        repository.save(ash);
+        repository.save(misty);
+    }
+
+    @AfterEach
+    void after(){
+        repository.deleteAll();
+    }
+
+    @Test
+    void getTrainers_shouldThrowAnUnauthorized(){
+        var responseEntity = this.restTemplate
+                .getForEntity("http://localhost:" + port + "/trainers/Ash", Trainer.class);
+        assertNotNull(responseEntity);
+        assertEquals(401, responseEntity.getStatusCodeValue());
+    }
+
     @Test
     void trainerController_shouldBeInstanciated(){
         assertNotNull(controller);
@@ -32,7 +77,9 @@ class TrainerControllerIntegrationTest {
 
     @Test
     void getTrainer_withNameAsh_shouldReturnAsh() {
-        var ash = this.restTemplate.getForObject("http://localhost:" + port + "/trainers/Ash", Trainer.class);
+        var ash = this.restTemplate
+                .withBasicAuth(username, password)
+                .getForObject("http://localhost:" + port + "/trainers/Ash", Trainer.class);
         assertNotNull(ash);
         assertEquals("Ash", ash.getName());
         assertEquals(1, ash.getTeam().size());
@@ -43,7 +90,9 @@ class TrainerControllerIntegrationTest {
 
     @Test
     void getAllTrainers_shouldReturnAshAndMisty() {
-        var trainers = this.restTemplate.getForObject("http://localhost:" + port + "/trainers/", Trainer[].class);
+        var trainers = this.restTemplate
+                .withBasicAuth(username, password)
+                .getForObject("http://localhost:" + port + "/trainers/", Trainer[].class);
         assertNotNull(trainers);
         assertEquals(2, trainers.length);
 
@@ -51,72 +100,88 @@ class TrainerControllerIntegrationTest {
         assertEquals("Misty", trainers[1].getName());
     }
 
-//    @Test
-//    void addTrainer() {
-//        String json = "{\"name\": \"Bug Catcher\",\"team\": [{\"pokemonType\": 13, \"level\": 6},{\"pokemonType\": 10, \"level\": 6}]}";
-//
-//        HttpHeaders headers = new HttpHeaders();
-//        headers.setContentType(MediaType.APPLICATION_JSON);
-//
-//        HttpEntity<String> entity = new HttpEntity<String>(json, headers);
-//
-//        var bugCatcher = this.restTemplate.postForObject("http://localhost:" + port + "/trainers/", entity, Trainer.class);
-//
-//        assertNotNull(bugCatcher);
-//        assertEquals("Bug Catcher", bugCatcher.getName());
-//        assertEquals(2, bugCatcher.getTeam().size());
-//
-//        assertEquals(13, bugCatcher.getTeam().get(0).getPokemonType());
-//        assertEquals(10, bugCatcher.getTeam().get(1).getPokemonType());
-//        assertEquals(6, bugCatcher.getTeam().get(0).getLevel());
-//        assertEquals(6, bugCatcher.getTeam().get(1).getLevel());
-//    }
-//
-//    @Test
-//    void updateTrainer() {
-//        String json = "{\"name\": \"to_update\",\"team\": [{\"pokemonType\": 13, \"level\": 6},{\"pokemonType\": 10, \"level\": 6}]}";
-//        String json2 = "{\"name\": \"to_update\",\"team\": [{\"pokemonType\": 14, \"level\": 6},{\"pokemonType\": 10, \"level\": 6}]}";
-//
-//        HttpHeaders headers = new HttpHeaders();
-//        headers.setContentType(MediaType.APPLICATION_JSON);
-//
-//        HttpEntity<String> entity = new HttpEntity<String>(json, headers);
-//        HttpEntity<String> entity2 = new HttpEntity<String>(json2, headers);
-//
-//        var toUpdate = this.restTemplate.postForObject("http://localhost:" + port + "/trainers/", entity, Trainer.class);
-//
-//        assertNotNull(toUpdate);
-//        assertEquals(13, toUpdate.getTeam().get(0).getPokemonType());
-//
-//        this.restTemplate.put("http://localhost:" + port + "/trainers/", entity2, Trainer.class);
-//
-//        var toUpdateAfter = this.restTemplate.getForObject("http://localhost:" + port + "/trainers/to_update", Trainer.class);
-//        assertNotNull(toUpdateAfter);
-//        assertEquals(14, toUpdateAfter.getTeam().get(0).getPokemonType());
-//    }
-//
-//    @Test
-//    void deleteTrainer() {
-//        String json = "{\"name\": \"to_delete\",\"team\": [{\"pokemonType\": 13, \"level\": 6},{\"pokemonType\": 10, \"level\": 6}]}";
-//
-//        HttpHeaders headers = new HttpHeaders();
-//        headers.setContentType(MediaType.APPLICATION_JSON);
-//
-//        HttpEntity<String> entity = new HttpEntity<String>(json, headers);
-//
-//        var toDelete = this.restTemplate.postForObject("http://localhost:" + port + "/trainers/", entity, Trainer.class);
-//
-//        assertNotNull(toDelete);
-//        assertEquals("to_delete", toDelete.getName());
-//
-//        var toDeleteBefore = this.restTemplate.getForObject("http://localhost:" + port + "/trainers/to_delete", Trainer.class);
-//        assertNotNull(toDeleteBefore);
-//
-//        this.restTemplate.delete("http://localhost:" + port + "/trainers/to_delete");
-//
-//        var toDeleteAfter = this.restTemplate.getForObject("http://localhost:" + port + "/trainers/to_delete", Trainer.class);
-//        assertNull(toDeleteAfter);
-//    }
+    @Test
+    void addTrainer() {
+        String json = "{\"name\": \"Bug Catcher\",\"team\": [{\"pokemonType\": 13, \"level\": 6},{\"pokemonType\": 10, \"level\": 6}]}";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<String> entity = new HttpEntity<String>(json, headers);
+
+        var bugCatcher = this.restTemplate
+                .withBasicAuth(username, password)
+                .postForObject("http://localhost:" + port + "/trainers/", entity, Trainer.class);
+
+        assertNotNull(bugCatcher);
+        assertEquals("Bug Catcher", bugCatcher.getName());
+        assertEquals(2, bugCatcher.getTeam().size());
+
+        assertEquals(13, bugCatcher.getTeam().get(0).getPokemonType());
+        assertEquals(10, bugCatcher.getTeam().get(1).getPokemonType());
+        assertEquals(6, bugCatcher.getTeam().get(0).getLevel());
+        assertEquals(6, bugCatcher.getTeam().get(1).getLevel());
+    }
+
+    @Test
+    void updateTrainer() {
+        String json = "{\"name\": \"to_update\",\"team\": [{\"pokemonType\": 13, \"level\": 6},{\"pokemonType\": 10, \"level\": 6}]}";
+        String json2 = "{\"name\": \"to_update\",\"team\": [{\"pokemonType\": 14, \"level\": 6},{\"pokemonType\": 10, \"level\": 6}]}";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<String> entity = new HttpEntity<String>(json, headers);
+        HttpEntity<String> entity2 = new HttpEntity<String>(json2, headers);
+
+        var toUpdate = this.restTemplate
+                .withBasicAuth(username, password)
+                .postForObject("http://localhost:" + port + "/trainers/", entity, Trainer.class);
+
+        assertNotNull(toUpdate);
+        assertEquals(13, toUpdate.getTeam().get(0).getPokemonType());
+
+        this.restTemplate
+                .withBasicAuth(username, password)
+                .put("http://localhost:" + port + "/trainers/", entity2, Trainer.class);
+
+        var toUpdateAfter = this.restTemplate
+                .withBasicAuth(username, password)
+                .getForObject("http://localhost:" + port + "/trainers/to_update", Trainer.class);
+        assertNotNull(toUpdateAfter);
+        assertEquals(14, toUpdateAfter.getTeam().get(0).getPokemonType());
+    }
+
+    @Test
+    void deleteTrainer() {
+        String json = "{\"name\": \"to_delete\",\"team\": [{\"pokemonType\": 13, \"level\": 6},{\"pokemonType\": 10, \"level\": 6}]}";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<String> entity = new HttpEntity<String>(json, headers);
+
+        var toDelete = this.restTemplate
+                .withBasicAuth(username, password)
+                .postForObject("http://localhost:" + port + "/trainers/", entity, Trainer.class);
+
+        assertNotNull(toDelete);
+        assertEquals("to_delete", toDelete.getName());
+
+        var toDeleteBefore = this.restTemplate
+                .withBasicAuth(username, password)
+                .getForObject("http://localhost:" + port + "/trainers/to_delete", Trainer.class);
+        assertNotNull(toDeleteBefore);
+
+        this.restTemplate
+                .withBasicAuth(username, password)
+                .delete("http://localhost:" + port + "/trainers/to_delete");
+
+        var toDeleteAfter = this.restTemplate
+                .withBasicAuth(username, password)
+                .getForObject("http://localhost:" + port + "/trainers/to_delete", Trainer.class);
+        assertNull(toDeleteAfter);
+    }
 
 
 }
